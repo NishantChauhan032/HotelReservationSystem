6917f837990045bb9725ec1fbb490f58865cd8e7
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-20 17:14:08 +0530

    [NISHANT] add : ability to add weekday and weekend rates for each hotel

--- src/main/java/com/capg/JUnit/hotelReservation/HotelReservationSystem.java
+++ src/main/java/com/capg/JUnit/hotelReservation/HotelReservationSystem.java
@@ -7,27 +7,20 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class HotelReservationSystem {
-	private static ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
+	public ArrayList<Hotel> hotelList;
 	private static Scanner sc = new Scanner(System.in);
-	private static Date checkInDate, checkOutDate;
+	public Date checkInDate, checkOutDate;
 
-	public ArrayList<Hotel> getHotelList() {
-		return hotelList;
+	public HotelReservationSystem() {
+		hotelList = new ArrayList<Hotel>();
 	}
 
-	public static void addHotel() {
-		System.out.println("Enter Name Of the Hotel : ");
-		String name = sc.nextLine();
-		System.out.println("Enter Weekday price of the Hotel : ");
-		int weekdayPrice = Integer.parseInt(sc.nextLine());
-		System.out.println("Enter Weekend price of the Hotel : ");
-		int weekendPrice = Integer.parseInt(sc.nextLine());
-
+	public void addHotel(String name, int weekdayPrice, int weekendPrice) {
 		Hotel newHotel = new Hotel(name, weekdayPrice, weekendPrice);
 		hotelList.add(newHotel);
 	}
 
-	public static String findCheapestHotel() {
+	public String findCheapestHotel() {
 
 		SimpleDateFormat dateFormat = new SimpleDateFormat("ddMMMyyyy");
 		System.out.println("Enter Check-In date(ddMMMyyyy),Check-Out date(ddMMMyyyy):");
@@ -35,12 +28,12 @@ public class HotelReservationSystem {
 
 		String[] dates = dateInput.split(","); // removes , and store dates in an array
 		try {
-			checkInDate = dateFormat.parse(dates[0]); //putting check in date at 0th index
+			checkInDate = dateFormat.parse(dates[0]); // putting check in date at 0th index
 		} catch (ParseException e) {
 			System.out.println("Please Enter a Valid CheckIn Date!");
 		}
 		try {
-			checkOutDate = dateFormat.parse(dates[1]); //putting checkout date at 1st index
+			checkOutDate = dateFormat.parse(dates[1]); // putting checkout date at 1st index
 		} catch (ParseException e) {
 			System.out.println("Please Enter A Valid CheckOut Date!");
 		}
@@ -63,15 +56,5 @@ public class HotelReservationSystem {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Hotel Reservation System!");
-		Hotel lakeWood = new Hotel("Lakewood", 110, 90);
-		Hotel bridgeWood = new Hotel("Bridgewood", 160, 50);
-		Hotel ridgeWood = new Hotel("Ridgewood", 220, 150);
-
-		hotelList.add(lakeWood);
-		hotelList.add(bridgeWood);
-		hotelList.add(ridgeWood);
-
-		System.out.println(hotelList);
-		findCheapestHotel();
 	}
 }
--- src/test/java/com/capg/JUnit/hotelReservation/HotelReservationSystemTest.java
+++ src/test/java/com/capg/JUnit/hotelReservation/HotelReservationSystemTest.java
@@ -1,7 +1,5 @@
 package com.capg.JUnit.hotelReservation;
 
-import java.util.ArrayList;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -11,19 +9,22 @@ public class HotelReservationSystemTest {
 	public void givenDetailsOfHotels_whenAddedToList_shouldShowSizeOfListAs3() {
 		HotelReservationSystem hotelObject = new HotelReservationSystem();
 
-		Hotel lakeWood = new Hotel("Lakewood", 110, 90);
-		Hotel bridgeWood = new Hotel("Bridgewood", 160, 50);
-		Hotel ridgeWood = new Hotel("Ridgewood", 220, 150);
+		hotelObject.addHotel("Lakewood", 110, 90);
+		hotelObject.addHotel("Lakewood", 160, 50);
+		hotelObject.addHotel("Lakewood", 220, 150);
+		int listSize = hotelObject.hotelList.size();
+		Assert.assertEquals(3, listSize); // Test Case For Use Case 1
+	}
 
-		ArrayList<Hotel> hotelList1 = hotelObject.getHotelList();
-		hotelList1.add(lakeWood);
-		hotelList1.add(bridgeWood);
-		hotelList1.add(ridgeWood);
-		int listSize = hotelList1.size();
-		
-		Assert.assertEquals(3, listSize); //Test Case For Use Case 1
+	@Test
+	public void givenDetailsOfThreeHotels_whenEnteredCorrect_shouldReturnCheapestHotel() {
+		HotelReservationSystem hotelObject = new HotelReservationSystem();
 
-		Assert.assertEquals("Lakewood", HotelReservationSystem.findCheapestHotel()); // Test Case For Use case 2
+		hotelObject.addHotel("Lakewood", 110, 90);
+		hotelObject.addHotel("Lakewood", 160, 50);
+		hotelObject.addHotel("Lakewood", 220, 150);
+
+		Assert.assertEquals("Lakewood", hotelObject.findCheapestHotel());// Test Case For Use Case 2
 	}
 
 }
